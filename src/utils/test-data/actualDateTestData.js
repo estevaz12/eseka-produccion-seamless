c6ad860eb4cd059245d1f8e3cf939eb4dd3af1d9
Esteban Vazquez Cardona <estebanvazquez1607@gmal.com>
2025-10-03 11:27:28 -0300

    fixed date estimate

--- src/utils/test-data/actualDateTestData.js
+++ src/utils/test-data/actualDateTestData.js
@@ -1,13 +1,7 @@
 module.exports = [
   {
-    Date: '2025-08-01T00:00:00.000Z',
-    Month: 8,
-    Year: 2025,
-    RoomCode: 'HOMBRE',
-  },
-  {
-    Date: '2025-08-01T00:00:00.000Z',
-    Month: 8,
+    Date: '2025-10-01T00:00:00.000Z',
+    Month: 10,
     Year: 2025,
     RoomCode: 'SEAMLESS',
   },
--- src/utils/test-data/dailyProductionTestData.js
+++ src/utils/test-data/dailyProductionTestData.js
@@ -1,14 +1,10 @@
 module.exports = [
   {
-    ProdDate: '2025-09-01T00:00:00.000Z',
-    Docenas: 1767,
+    ProdDate: '2025-10-01T00:00:00.000Z',
+    Docenas: 937,
   },
   {
-    ProdDate: '2025-09-02T00:00:00.000Z',
-    Docenas: 1409,
-  },
-  {
-    ProdDate: '2025-09-03T00:00:00.000Z',
-    Docenas: 1484,
+    ProdDate: '2025-10-02T00:00:00.000Z',
+    Docenas: 1117,
   },
 ];
